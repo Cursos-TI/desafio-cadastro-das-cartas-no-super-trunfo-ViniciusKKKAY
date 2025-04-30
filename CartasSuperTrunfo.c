9e10b5bd540341dbf3e6b534e6ab174cd3263906
ViniciusKKKAY <119528569+ViniciusKKKAY@users.noreply.github.com>
2025-04-30 14:19:23 -0400

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,9 +14,9 @@ int main() {
     printf("Digite o Estado:\n");
     scanf(" %c", &Estado);
     printf("Digite o Código da Carta:\n");
-    scanf("%3s", Cod_carta);
+    scanf("%s", Cod_carta);
     printf("Digite o Nome da Cidade:\n");
-    scanf(" %[^\n]", nome_cidade);
+    scanf(" %s", nome_cidade);
     printf("Digite a quantidade de habitantes da cidade:\n");
     scanf("%d", &populacao);
     printf("Digite a Área (em km²) da cidade:\n");
@@ -30,9 +30,9 @@ int main() {
     printf("Digite o Estado:\n");
     scanf(" %c", &Estado2);
     printf("Digite o Código da Carta:\n");
-    scanf("%3s", Cod_carta2);
+    scanf("%s", Cod_carta2);
     printf("Digite o Nome da Cidade:\n");
-    scanf(" %[^\n]", nome_cidade2);
+    scanf(" %s", nome_cidade2);
     printf("Digite a quantidade de habitantes da cidade:\n");
     scanf("%d", &populacao2);
     printf("Digite a Área (em km²) da cidade:\n");
@@ -48,8 +48,8 @@ int main() {
     printf("Código da Carta: %s\n", Cod_carta);
     printf("Nome da Cidade: %s\n", nome_cidade);
     printf("População: %d\n", populacao);
-    printf("Área (em km²): %.2f\n", Area_km);
-    printf("PIB: %.2f\n", pib);
+    printf("Área (em km²): %f\n", Area_km);
+    printf("PIB: %f\n", pib);
     printf("Número de Pontos Turísticos: %d\n", Num_pont_turis);
 
     // Imprime as informações da segunda carta
@@ -58,8 +58,8 @@ int main() {
     printf("Código da Carta: %s\n", Cod_carta2);
     printf("Nome da Cidade: %s\n", nome_cidade2);
     printf("População: %d\n", populacao2);
-    printf("Área (em km²): %.2f\n", Area_km2);
-    printf("PIB: %.2f\n", pib2);
+    printf("Área (em km²): %f\n", Area_km2);
+    printf("PIB: %f\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", Num_pont_turis2);
 
     return 0;
