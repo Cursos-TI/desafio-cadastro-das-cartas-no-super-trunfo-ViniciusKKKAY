030cee56e00bda8e6faa3c23d16cbae6db64376d
ViniciusKKKAY <119528569+ViniciusKKKAY@users.noreply.github.com>
2025-04-30 21:52:39 -0400

    CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,11 +3,9 @@
 int main() {
     // Declaração de variáveis
     char Estado, Estado2;
-    char Cod_carta[4], Cod_carta2[4];
-    char nome_cidade[50], nome_cidade2[50];
+    char Cod_carta[4], Cod_carta2[4], nome_cidade[50], nome_cidade2[50];
     int populacao, populacao2;
-    float Area_km, Area_km2;
-    float pib, pib2;
+    float Area_km, Area_km2, pib, pib2, per_capita, Den_popu, per_capita2, Den_popu2;
     int Num_pont_turis, Num_pont_turis2;
 
     // Lendo os dados de entrada da primeira carta
@@ -25,6 +23,10 @@ int main() {
     scanf(" %f", &pib);
     printf("Digite o Número de Pontos Turísticos da cidade:\n");
     scanf(" %d", &Num_pont_turis);
+    Den_popu  = populacao / Area_km;
+    per_capita = pib / populacao;
+
+
 
     // Lendo os dados de entrada da segunda carta
     printf("Digite o Estado:\n");
@@ -41,6 +43,8 @@ int main() {
     scanf(" %f", &pib2);
     printf("Digite o Número de Pontos Turísticos da cidade:\n");
     scanf(" %d", &Num_pont_turis2);
+    Den_popu2  = populacao2 / Area_km2;
+    per_capita2 = pib2 / populacao2;
 
     // Imprime as informações da primeira carta
     printf("\nCarta1\n");
@@ -51,6 +55,8 @@ int main() {
     printf("Área (em km²): %f\n", Area_km);
     printf("PIB: %f\n", pib);
     printf("Número de Pontos Turísticos: %d\n", Num_pont_turis);
+    printf ("O Pib Per Capita é: %f\n",per_capita);
+    printf ("O número de habitantes por quilômetro quadrado é: %.2f\n", Den_popu);
 
     // Imprime as informações da segunda carta
     printf("\nCarta2\n");
@@ -61,6 +67,8 @@ int main() {
     printf("Área (em km²): %f\n", Area_km2);
     printf("PIB: %f\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", Num_pont_turis2);
+    printf ("O Pib Per Capita é: %f\n", per_capita2);
+    printf ("O número de habitantes por quilômetro quadrado é: %.2f\n", Den_popu2);
 
     return 0;
 }
